71eb36047db7989b2abe78270e50c8970c160c8b
Yuliya Grygoryeva <yuliyagrygoryeva@Yuliyas-MacBook-Pro.local>
2023-12-29 19:43:06 -0800

    Add empty project

---
+++ iTour/iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Yuliya Grygoryeva on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iTour/iTourUITests/iTourUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iTourUITestsLaunchTests.swift
+//  iTourUITests
+//
+//  Created by Yuliya Grygoryeva on 12/29/23.
+//
+
+import XCTest
+
+final class iTourUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
