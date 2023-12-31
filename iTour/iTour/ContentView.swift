bc34bcef220052f2aa02a3ac2aafd0a89e1cf8bd
Yuliya Grygoryeva <yuliyagrygoryeva@Yuliyas-MacBook-Pro.local>
2023-12-30 20:40:08 -0800

    Add search and filtering

--- iTour/iTour/ContentView.swift
+++ iTour/iTour/ContentView.swift
@@ -12,12 +12,14 @@ struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @State private var path = [Destination]()
     @State private var sortOrder = SortDescriptor(\Destination.name)
+    @State private var searchText = ""
     
     var body: some View {
         NavigationStack(path: $path) {
-            DestinationListingView(sort: sortOrder)
+            DestinationListingView(sort: sortOrder, searchString: searchText)
                 .navigationTitle("iTour")
                 .navigationDestination(for: Destination.self, destination: EditDestinationView.init)
+                .searchable(text: $searchText)
                 .toolbar {
                     Button("Add Samples", action: addSamples)
                     Button("Add Destination", systemImage: "plus", action: addDestination)
