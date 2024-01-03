ea00fd39e39617259b727cdbcdbaaf3f070d5951
Yuliya Grygoryeva <yuliyagrygoryeva@Yuliyas-MacBook-Pro.local>
2024-01-02 18:22:08 -0800

    Add sights, filtering

---
+++ iTour/iTour/Sight.swift
@@ -0,0 +1,18 @@
+//
+//  Sight.swift
+//  iTour
+//
+//  Created by Yuliya Grygoryeva on 12/30/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Sight {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
